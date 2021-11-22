88c81d11d71889675ad9a84078a9bc7ebcfdb738
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-22 20:10:14 +0100

    fixed menu finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,18 @@
 const overviewButton = document.querySelector(".overview");
 const structureButton = document.querySelector(".structure");
 const geologyButton = document.querySelector(".geology");
+const links = document.querySelector(".fix");
+const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const hamburgerImg = document.querySelector("#hamburgerBtn img");
+
+hamburgerBtn.addEventListener("click", () => {
+  links.classList.toggle("active");
+  hamburgerImg.src = "../../images/icon-hamburger.svg";
+
+  if (links.classList.contains("active")) {
+    hamburgerImg.src = "../../images/icon-close.svg";
+  }
+});
 
 overviewButton.addEventListener("click", () => {
   overviewButton.classList.add("active");
