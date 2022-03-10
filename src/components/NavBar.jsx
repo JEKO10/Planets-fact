8d412da1c968159d483db06922570e7c7546c18e
Aleksa Bubanja <aleksa3b@gmail.com>
2022-03-10 21:34:35 +0100

    border toggle for links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar({ color, setColor }) {
+  const [border, setBorder] = useState("");
+
+  const styles = {
+    borderTop: `5px solid ${border}`,
+  };
+
   return (
     <nav>
       <Link to="/">
@@ -12,8 +19,10 @@ function NavBar({ color, setColor }) {
       <div className="fix">
         <ul className="links">
           <li
+            style={border === "#def4fc" ? styles : {}}
             onClick={() => {
               setColor("#419db9");
+              setBorder("#def4fc");
             }}
           >
             <div id="ball"></div>
@@ -25,8 +34,10 @@ function NavBar({ color, setColor }) {
             </Link>
           </li>
           <li
+            style={border === "#f7cc7e" ? styles : {}}
             onClick={() => {
               setColor("#eda44a");
+              setBorder("#f7cc7e");
             }}
           >
             <div id="ball"></div>
@@ -38,8 +49,10 @@ function NavBar({ color, setColor }) {
             </Link>
           </li>
           <li
+            style={border === "#5358fb" ? styles : {}}
             onClick={() => {
               setColor("#6f2ed6");
+              setBorder("#5358fb");
             }}
           >
             <div id="ball"></div>
@@ -51,8 +64,10 @@ function NavBar({ color, setColor }) {
             </Link>
           </li>
           <li
+            style={border === "#ff6c47" ? styles : {}}
             onClick={() => {
               setColor("#d14e33");
+              setBorder("#ff6c47");
             }}
           >
             <div id="ball"></div>
@@ -64,8 +79,10 @@ function NavBar({ color, setColor }) {
             </Link>
           </li>
           <li
+            style={border === "#ecad79" ? styles : {}}
             onClick={() => {
               setColor("#d93b36");
+              setBorder("#ecad79");
             }}
           >
             <div id="ball"></div>
@@ -77,8 +94,10 @@ function NavBar({ color, setColor }) {
             </Link>
           </li>
           <li
+            style={border === "#fccb69" ? styles : {}}
             onClick={() => {
               setColor("#cb5020");
+              setBorder("#fccb69");
             }}
           >
             <div id="ball"></div>
@@ -90,8 +109,10 @@ function NavBar({ color, setColor }) {
             </Link>
           </li>
           <li
+            style={border === "#66f0d4" ? styles : {}}
             onClick={() => {
               setColor("#1ec2a4");
+              setBorder("#66f0d4");
             }}
           >
             <div id="ball"></div>
@@ -103,8 +124,10 @@ function NavBar({ color, setColor }) {
             </Link>
           </li>
           <li
+            style={border === "#477dfa" ? styles : {}}
             onClick={() => {
               setColor("#2e68f0");
+              setBorder("#477dfa");
             }}
           >
             <div id="ball"></div>
